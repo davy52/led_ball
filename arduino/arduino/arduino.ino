5f4603a2ac841f33380c6cc1bcd6630134724fb8
dutchi13 <59352297+dutchi13@users.noreply.github.com>
2020-08-12 00:07:37 +0200

    first files?

---
+++ arduino/arduino/arduino.ino
@@ -0,0 +1,18 @@
+#include <wire.h>
+
+const int MPU_ADDR = 0x68;
+
+int16_t acc_x, acc_y, acc_z;
+int16_t gyro_x, gyro_y, gyro_z;
+
+char buff[85];
+
+
+void setup() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
